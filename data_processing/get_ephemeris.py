5855dbd528960a514b44e0562f626532769e2c47
Nathan Besch <nathanbesch27@gmail.com>
2025-07-15 15:49:52 +0100

    Fix ephemeris not working

--- data_processing/get_ephemeris.py
+++ data_processing/get_ephemeris.py
@@ -4,6 +4,8 @@ import pandas as pd
 import datetime
 import tqdm
 
+spice.furnsh("SPICE/cassini/metakernel_cassini.txt")
+spice.furnsh("SPICE/cassini/kernels/fk/cas_dyn_v03.tf")
 unet_catalogue_csv_fp = 'data/raw/2004001_2017258_start_stop_times.csv'
 
 #   CASSINI end date: Sep. 15 2017
@@ -15,8 +17,6 @@ minutely_datetimes = np.arange(start_datetime, stop_datetime, datetime.timedelta
 
 def get_CassiniEphemeris(datetimes):
     """
-    
-
     Parameters
     ----------
     datetimes : list of datetimes
@@ -96,8 +96,8 @@ def get_CassiniEphemeris(datetimes):
     return df
 
 #   Print this to a csv (this will be pretty hefty)
-#eph_df = get_CassiniEphemeris(minutely_datetimes)
-#eph_df.to_csv('data/calculated/20040101000000_20170915115700_ephemeris.csv', index_label='datetime')
+eph_df = get_CassiniEphemeris(minutely_datetimes)
+eph_df.to_csv('data/calculated/20040101000000_20170915115700_ephemeris.csv', index_label='datetime')
 
 lfe_unet = pd.read_csv(unet_catalogue_csv_fp)
 startTimes = lfe_unet["start"]
